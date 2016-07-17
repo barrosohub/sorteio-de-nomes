6d7bab2b88484cf79791513216c8d5b51792e44f
Barroso Filho <barrosofilho.ti@gmail.com>
2016-07-17 01:51:37 -0300

    Create sorteioNome.php

---
+++ sorteioNome.php
@@ -0,0 +1,22 @@
+<?php
+
+function iniciar()
+{
+    function sortearNome()
+    {
+        $nome = ["Joao", "Maria", "Carla", "Marciana", "Luciana", "Margarete", "Claudiane", "Ramilo", "Felipe"];
+        $x = rand(0, (count($nome)-1));
+
+        return $a[] = $nome[$x] . "<br/>";
+
+    }
+
+    echo "Nome sorteado: <br/>";
+
+    for ($s = 1; $s <= 1; $s++)
+    {
+        echo $y[] = sortearNome();
+    }
+}
+
+iniciar();
